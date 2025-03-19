328e994137bf65352b1f9940048e8005f18a63b4
DESKTOP-0ESSPKQ\WIN11 <phanphongza23@gmail.com>
2025-03-19 13:25:28 +0700

    google logon

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -6,6 +6,13 @@ use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Session;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Firebase\JWT\JWT;
+use GuzzleHttp\Client;
+use Google_Client;
+use Exception;
+use Illuminate\Support\Facades\Http; // นำเข้า Http Facade
+
 
 class GoogleAuthController extends Controller
 {
@@ -13,82 +20,85 @@ class GoogleAuthController extends Controller
     public function redirect()
     {
         return Socialite::driver('google')
-            ->scopes(['email', 'profile'])
+            ->scopes(['openid', 'email', 'profile'])
             ->with(['prompt' => 'consent']) // บังคับให้ขอสิทธิ์ใหม่
             ->redirect();
     }
     // 2. รับ Callback จาก Google
     public function callback()
     {
-
         $googleUser = Socialite::driver('google')->user();
-      
-        $googleUserArray = [
-            'id' => $googleUser->getId(),
-            'name' => $googleUser->getName(),
-            'email' => $googleUser->getEmail(),
-            'avatar' => $googleUser->getAvatar(),
-            'token' => $googleUser->token,
-        ];
-       
-
-
-        $openId = env('openID');
-        $productCode = env('productCode');
-        $openType = 8;
-        $userOpenId  = "CREDENTIAL";
-        $access_token = $googleUserArray['token'];
-        $openKey = env('openKey');
-
-        $params = [
-            'openId' => $openId,
-            'productCode' => $productCode,
-            'openType' => $openType,
-            'userOpenId' => $userOpenId,
-            'access_token' => $access_token
-        ];
-
-
-      
-        // คำนวณค่า MD5 sign
-        $sign = $this->getMd5Sign($params, $openKey);
-        $params['sign'] = $sign;
-
-       
-        
-        // API URL
-        $url = env('URL_SDK') . "open/snsLogin";
-
-        // ส่ง API
-        $response = $this->sendPostRequest($url, $params);
+        $response = $googleUser->accessTokenResponseBody;
+        $idToken = $response['id_token']; // ID 
         echo "<pre>";
-        print_r($response);
-      
-        // Session::put('authenticated', true);
+        print_r($googleUser);
+        exit();
+        // echo "<br>";
+        // $googleUserArray = [
+        //     'id' => $googleUser->getId(),
+        //     'name' => $googleUser->getName(),
+        //     'email' => $googleUser->getEmail(),
+        //     'avatar' => $googleUser->getAvatar(),
+        //     'token' => $googleUser->token,   // Access Token from Google
+        // ];
 
+        // // Secret key for signing the JWT
+        // $key = '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';
 
-        // return redirect()->route('home');
-        // $data = json_decode($response, true);
+        // // Algorithm used for signing
+        // $algorithm = 'HS256';
+
         // echo "<pre>";
-       
-        // // ถ้าไม่มีให้สร้างใหม่
-        // if (!$user) {
-        //     $user = User::create([
-        //         'name' => $googleUser->getName(),
-        //         'email' => $googleUser->getEmail(),
-        //         'google_id' => $googleUser->getId(),
-        //         'password' => bcrypt('google_auth'), // ตั้งค่าให้ปลอดภัย
-        //     ]);
-        // }
+        // print_r($googleUser->token);
+        // echo "<br>";
+        // // Payload containing the token and other claims
+        // $payload = [
+        //     'token' => $googleUser->token, // Google access token
+        //     'iat' => time(),               // Issued at time
+        //     'exp' => time() + 3600,         // Expiration time (1 hour)
+        // ];
 
-        // // เข้าสู่ระบบผู้ใช้
-        // Auth::login($user);
-        // Session::put('authenticated', true);
-
-        // return redirect('/');
+        // Encode the JWT using the secret key and chosen algorithm
+        try {
+            $jwt = JWT::encode($payload, $key, $algorithm);
 
 
 
-        // return redirect()->route('home');
+            $openId = env('openID');
+            $productCode = env('productCode');
+            $openType = 8;
+            $userOpenId  = "CREDENTIAL";
+            $access_token = $jwt;
+            $openKey = env('openKey');
+
+            $params = [
+                'openId' => $openId,
+                'productCode' => $productCode,
+                'openType' => $openType,
+                'userOpenId' => $userOpenId,
+                'access_token' => $access_token
+            ];
+
+            // คำนวณค่า MD5 sign
+            $sign = $this->getMd5Sign($params, $openKey);
+            $params['sign'] = $sign;
+
+
+            print_r($params);
+            // API URL
+            $url = env('URL_SDK') . "open/snsLogin";
+
+            // ส่ง API
+            $response = $this->sendPostRequest($url, $params);
+
+            print_r($response);
+
+            $res = json_decode($response, true);
+        } catch (Exception $e) {
+            echo "Error generating JWT: " . $e->getMessage();
+        }
     }
+
 }
+
+
